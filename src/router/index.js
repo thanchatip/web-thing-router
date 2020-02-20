2216fa8861081b5f8acbdce2ab8ffc0da1a2350b
thanchatip <thanchatip.coe@gmail.com>
2020-02-20 15:21:12 +0700

    :re: adjust code

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/event',
+    name: 'Event',
+    component: Event
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
